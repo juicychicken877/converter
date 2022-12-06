167bbb808e49527a70751118eafb3eeee4f26922
Krystian Stanik <110622492+juicychicken877@users.noreply.github.com>
2022-12-06 20:10:59 +0100

    update
    + 2 variables from convert.cpp

--- converter app/code/convert.hpp
+++ converter app/code/convert.hpp
@@ -1,21 +1,21 @@
 #include <iostream>
-
 using namespace std;
 
 class Convert 
 {
 private:
-    string binary, hexadecimal, octal; //binary only used to convert decimal to binary
+    string binary, hexadecimal, octal; //binary variable only used to convert decimal to binary
+    string hex_binary, oct_binary;
     unsigned long long decimal=0, bit=1, byte_sum=0;
 
 public:
     //converting functions
     string dec_to_bin(unsigned long long dec);  //decimal to binary
     string hex_to_bin(string hex);             //hexadecimal to binary
-    string oct_to_bin(string oct);              //octal to binary
-    unsigned long long bin_to_dec(string bin); //binary to decimal
-    string bin_to_hex(string bin);            //binary to hexadecimal
-    string bin_to_oct(string bin);              //binary to octal
+    string oct_to_bin(string oct);            //octal to binary
+    unsigned long long bin_to_dec(string bin);//binary to decimal
+    string bin_to_hex(string bin);          //binary to hexadecimal
+    string bin_to_oct(string bin);         //binary to octal
     void reset();
 
 };
