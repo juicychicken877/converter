19aa131fd55c94d5247d8286bde24c2b220f73ee
Krystian Stanik <110622492+juicychicken877@users.noreply.github.com>
2022-12-06 20:08:57 +0100

    update
    + error control
    + from 1 to all conversion

--- converter app/code/main.cpp
+++ converter app/code/main.cpp
@@ -1,84 +1,94 @@
 #include <iostream>
-#include "convert.hpp"
 #include <windows.h>
+#include <conio.h>
+#include "convert.hpp"
 using namespace std;
 
 void show_menu()
 {
-    cout << "------------------" << endl;
-    cout << "1. DEC to BIN" << endl;
-    cout << "2. HEX to BIN" << endl;
-    cout << "3. OCT to BIN" << endl;
-    cout << "4. BIN to DEC" << endl;
-    cout << "5. BIN to HEX" << endl;
-    cout << "6. BIN to OCT" << endl;
-    cout << "7. EXIT" << endl;
-    cout << "Your option: ";
+    cout << "---------------------------" << endl
+         << "CONVERT :"                   << endl
+         << "1. DECIMAL NUMBER"           << endl
+         << "2. BINARY NUMBER"            << endl
+         << "3. HEXADECIMAL NUMBER"       << endl
+         << "4. OCTAL NUMBER"             << endl
+         << "5. EXIT"                     << endl
+         << "Your option: ";
 }
-//----------------------------------------------------
+
 int main()
 {
-    int main_menu_option; 
+    char main_menu_option; 
     char continue_option;
     string string_num;  unsigned long long int_num; //nums to convert
+    string container;
 
-    while (main_menu_option != 7)
+    while (main_menu_option != 5)
     {
         system("CLS");
 
         show_menu();
 
-        cin >> main_menu_option;
+        main_menu_option = getch(); //input
+        cout << main_menu_option << endl;
 
-        Convert convert; //converting object
+        main_menu_option = (int)(main_menu_option-48); //in case if someone try to enter a character instead of number
 
-        convert.reset(); //reset all of the values
+        if (main_menu_option >= 1 && main_menu_option <= 5)
+        { 
+            Convert convert; //converting object
 
-        cout << "Type a number: ";
+            switch (main_menu_option)
+            {
+                case 1: //dec to all
+                    cout << "(10) : "; cin >> int_num;
+                    container = convert.dec_to_bin(int_num);
+                        
+                    cout << "(2)  : " << container << endl;                     //dec to bin
+                    cout << "(8)  : " << convert.bin_to_oct(container) << endl; //converted bin from dec to oct
+                    cout << "(16) : " << convert.bin_to_hex(container) << endl; //converted bin from dec to hex
+                break; 
+                case 2: //bin to all
+                    cout << "(2)  : "; cin >> string_num;
+                    cout << "(8)  : " << convert.bin_to_oct(string_num) << endl;
+                    cout << "(10) : " << convert.bin_to_dec(string_num) << endl;
+                    convert.reset();
+                    cout << "(16) : " << convert.bin_to_hex(string_num) << endl;
+                break;
+                case 3: //hex to all
+                    cout << "(16) : "; cin >> string_num;
+                    container = convert.hex_to_bin(string_num);
 
-        if (main_menu_option == 1)  //depending on option, read the right variable (int or string);
-            cin >> int_num;
-        else if (main_menu_option > 1 && main_menu_option < 7)
-            cin >> string_num;
+                    cout << "(2)  : " << container << endl;
+                    cout << "(8)  : " << convert.bin_to_oct(container) << endl;
+                    cout << "(10) : " << convert.bin_to_dec(container) << endl;
+                break;
+                case 4: //oct to all
+                    cout << "(8)  : "; cin >> string_num;
+                    container = convert.oct_to_bin(string_num);
 
-        switch (main_menu_option)
-        {
-            case 1: 
-                cout << "dec to bin: " << convert.dec_to_bin(int_num) << endl;
-            break; 
-            case 2: 
-                cout << "hex to bin: " << convert.hex_to_bin(string_num) << endl;
-            break;
-            case 3:
-                cout << "oct to bin: " << convert.oct_to_bin(string_num) << endl; 
-            break;
-            case 4:
-                cout << "bin to dec: " << convert.bin_to_dec(string_num) << endl;
-            break;
-            case 5: 
-                cout << "bin to hex: " << convert.bin_to_hex(string_num) << endl;
-            break;
-            case 6:
-                cout << "bin to oct: " << convert.bin_to_oct(string_num) << endl;
-            break;
-            case 7: 
-                exit(0); 
-            break;
-        }
-        cout << "------------------" << endl;
-        cout << "Continue? y/n : "; cin >> continue_option;
+                    cout << "(2)  : " << container << endl
+                         << "(10) : " << convert.bin_to_dec(container) << endl;
+                    convert.reset();
+                    cout << "(16) : " << convert.bin_to_hex(container) << endl;
+                break;
+                case 5: exit(0); break;
+            }
+
+            cout << "---------------------------" << endl
+                 << "Continue? y/n : "; cin >> continue_option;
         
-        switch (continue_option) 
-        {
-            case 'y': 
-                continue; 
-            break;
-            case 'n': 
-                exit(0); 
-            break;
-            default: 
-                continue; 
-            break;
+            if (continue_option =='n') 
+                exit(0);
+            else 
+                continue;
         }
+        else 
+        {
+            cout << "!!! Please choose option 1 - 5 !!!" << endl;
+            Sleep(2000);
+        }
+
     }
+}
 }
